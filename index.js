5e35187d0696785bccd3f105c257c6a1157bb975
Garrett Pfingston <gpfingston@gmail.com>
2025-10-07 23:39:10 -0500

    Added functions for lab

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
+function calculateTax(amount){
+    let taxAmount = amount / 10;
+    return taxAmount;
+}
 
+function convertToUpperCase(text){
+    convertedString = text.toUpperCase();
+    return convertedString;
+}
 
+function findMaximum (num1, num2){
+    if (num1 >= num2){
+        return num1;
+    } else if (num2 > num1){
+        return num2;
+    }
+}
 
+function isPalindrome(word){
+    return word == word.split('').reverse().join('');
+}
+
+function calculateDiscountedPrice(originalPrice, discountPercentage){
+    discountedPrice = originalPrice - (originalPrice * (discountPercentage / 100));
+    return discountedPrice;
+}
 
 
 // This is required for the test to function properly  
